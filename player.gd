949c69aabb009c1eb942c5f07fdbe8bbb56da67e
doryan.dahon <doryan.dahon@mydiapason.com>
2025-03-22 21:13:12 +0100

    added score

--- player.gd
+++ player.gd
@@ -2,9 +2,16 @@ extends Node2D
 
 @onready var paddle: Paddle = $Paddle
 
-@export var speed: float = 500
+@export var speed: float = 1000
 @export var up_control: StringName
 @export var down_control: StringName
 
+var score: int = 0
+
 func _physics_process(delta):
 	paddle.velocity.y = Input.get_axis(up_control, down_control) * speed
+
+
+func _on_left_border_body_entered(body: Node2D) -> void:
+	score += 1
+	print_debug(score)
